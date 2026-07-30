8ee8de8e7a043bc3819007a41188aea72b10fd45
Herin Patel <herinp06@gmail.com>
2026-07-31 00:32:01 +0530

    Add functions that take password input, hash password, verify password

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import secrets
+import bcrypt
 
 def main():
     pass
@@ -18,4 +19,20 @@ def verify_otp(user_input_otp, generated_otp):
         return 1
     else:
         return 0
-    
+
+def user_input_password():
+    password = input("Enter password: ")
+    return password
+
+def hash_password(password):
+    pass_bytes = password.encode('UTF-8')
+    salt_rounds = 12
+    salt = bcrypt.gensalt(salt_rounds)
+    hash_bytes = bcrypt.hashpw(pass_bytes, salt)
+    hash_pass = hash_bytes.decode('UTF-8')
+    return hash_pass
+
+def verify_password(password1, password2):
+    pass1 = password1.encode('UTF-8')
+    pass2 = password2.encode('UTF-8')
+    return bcrypt.checkpw(pass1, pass2)
