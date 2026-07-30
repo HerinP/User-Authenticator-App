60d9844acd5142848cd15437fb7b75e46049059f
Herin Patel <herinp06@gmail.com>
2026-07-30 19:27:18 +0530

    Add function that prompts user to input details

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    pass
+
+def user_details():
+    user_name = input("Enter your name: ")
+    user_email = input("Enter your email: ")
+    user_details_list = [user_name, user_email]
+    return user_details_list
