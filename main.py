21dee523121d2bf300579242e68fc9e30764dd9f
Herin Patel <herinp06@gmail.com>
2026-07-30 20:51:30 +0530

    Add function that verifies user input otp and generated otp using secrets

--- main.py
+++ main.py
@@ -12,3 +12,10 @@ def user_details():
 def generate_otp():
     otp = secrets.randbelow(900000) + 100000
     return otp
+
+def verify_otp(user_input_otp, generated_otp):
+    if secrets.compare_digest(user_input_otp, generated_otp):
+        return 1
+    else:
+        return 0
+    
