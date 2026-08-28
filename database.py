7b1034a0000e427430111f403fc74fe6b69ade07
Herin Patel <herinp06@gmail.com>
2026-08-28 15:01:40 +0530

    Refactor email existence query

--- database.py
+++ database.py
@@ -21,7 +21,7 @@ def insert_user_details_db(db_connection_object, user_name, user_email, user_pas
 def email_exists(db_connection_object, email):
     """This function checks that email exists in database or not"""
     cursor = db_connection_object.cursor()
-    sql = "SELECT * FROM user WHERE email = %s"
+    sql = "SELECT 1 FROM user WHERE email = %s"
     values = (email,)
     cursor.execute(sql, values)
     result = cursor.fetchone()
