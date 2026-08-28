e5513eee6be09675ca1f9adb2fb9489d10174833
Herin Patel <herinp06@gmail.com>
2026-08-28 15:12:11 +0530

    Fix value error at user input otp and Fix mistake on config file

--- otp.py
+++ otp.py
@@ -12,6 +12,8 @@ def time_limit_input(timeout):
     """This function waits for user to input for a limited time"""
     try:
         return int(inputimeout("Enter otp: ", timeout))
+    except ValueError:
+        print("OTPs are in Integer format!")
     except TimeoutOccurred:
         print("OTP expired")
 
